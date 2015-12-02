7045b7fa7f5c12c61ec6116b92422fd4a85326fd
Md. Nasir Uddin <mail2freelancernasir@gmail.com>
2015-12-02 17:15:17 +0600

    Uploaded FoodPanda project

---
+++ contact.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Lucky idea">
+    <meta name="keywords" content="Food Panda, Restaurant, Food, Foods">
+    <meta name="author" content="https://www.foodpanda.com.bd/">
+    <meta http-equiv="refresh" content="600">
+
+    <link rel="stylesheet" href="common/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="common/font-awesome-4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="common/css/emythmakers.css">
+</head>
+<body>
+<header class="navbar navbar-inverse navbar-static-top navbar-fixed-top" id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-xs-6">
+                <div class="navbar-header">
+                    <a href="#" class=""><img src="media/common/Food24x7%20Logo.png" alt="Logo" width="150
+						"></a>
+                </div>
+            </div>
+            <nav class="col-sm-6 col-xs-6">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden-xs"><a href="#">About</a></li>
+                    <li class="hidden-xs"><a href="#">Gallery</a></li>
+                    <li class="hidden-xs"><a href="#">Contact</a></li>
+                    <li class="hidden-xs"><a href="#">Login</a></li>
+                    <li class="visible-xs"><a href="#"><i class="fa fa-unlink"></i></a></li>
+                    <li class="visible-xs"><a href="#"><i class="fa fa-photo"></i></a></li>
+                    <li class="visible-xs"><a href="#"><i class="fa fa-mobile-phone"></i></a></li>
+                    <li class="visible-xs"><a href="#"><i class="fa fa-user"></i></a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="about">
+    <div class="container">
+        <div class="row">
+            <!-- start contact -->
+            <div class="col-sm-6">
+                <form action="">
+                    <label for="name">Name</label>
+                    <input type="text" name="" class="form-control" id="name" placeholder="First"/>
+
+                    <label for="email">Email</label>
+                    <input type="email" name=""  class="form-control" id="email" placeholder="Email address"/>
+
+                    <label for="message">Message</label>
+                    <textarea name="" id="message"  class="form-control" cols="30" rows="10" placeholder="Type here..."></textarea>
+
+                    <label for="submit"></label>
+                    <input type="submit" value="Send Email" class="btn btn-success" id="submit"/>
+                </form>
+            </div>
+            <div class="col-sm-6">
+                <div class="contact-map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3741747.5548836067!2d88.10025525264989!3d23.674157460936794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30adaaed80e18ba7%3A0xf2d28e0c4e1fc6b!2sBangladesh!5e0!3m2!1sen!2s!4v1445577594674" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+                <address id="address">
+                    <p>
+                        <b>Call:</b> +00825874125
+                    </p>
+                    <p>
+                        <b>Email:</b>hello@hello.com
+                    </p>
+                    <p>
+                        <b>Visit:</b>
+                        Dhanmondi. Dhaka. <br /> Bangladesh.
+                    </p>
+                    <p>
+                        <b>Social:</b>
+                        <i class="fa fa-facebook"></i>
+                        <i class="fa fa-twitter"></i>
+                        <i class="fa fa-google-plus"></i>
+                        <i class="fa fa-linkedin"></i>
+                        <i class="fa fa-youtube"></i>
+                    </p>
+                </address>
+            </div><!--/ close contact-form -->
+        </div>
+    </div>
+
+</div>
+<footer id="footer-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                All rights reserved by <a href="#">emythmakers.com</a>.2015
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script type="text/javascript" src="common/jQuery-2.1.4/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="common/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="common/js/emythmakers.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,547 @@
+﻿<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Food Hunger</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="description" content="Lucky idea">
+<meta name="keywords" content="Food Panda, Restaurant, Food, Foods">
+<meta name="author" content="https://www.foodpanda.com.bd/">
+<meta http-equiv="refresh" content="600">
+
+<link rel="stylesheet" href="common/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="common/font-awesome-4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="common/css/emythmakers.css">
+</head>
+<body>
+	<header class="navbar navbar-inverse navbar-static-top navbar-fixed-top" id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-xs-6">
+					<div class="navbar-header">
+						<a href="#" class=""><img src="media/common/Food24x7%20Logo.png" alt="Logo" width="150
+						"></a>
+					</div>
+				</div>
+				<nav class="col-sm-6 col-xs-6">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="hidden-xs"><a href="#">About</a></li>
+						<li class="hidden-xs"><a href="#">Gallery</a></li>
+						<li class="hidden-xs"><a href="#">Contact</a></li>
+						<li class="hidden-xs"><a href="#">Login</a></li>
+						<li class="visible-xs"><a href="#"><i class="fa fa-unlink"></i></a></li>
+						<li class="visible-xs"><a href="#"><i class="fa fa-photo"></i></a></li>
+						<li class="visible-xs"><a href="#"><i class="fa fa-mobile-phone"></i></a></li>
+						<li class="visible-xs"><a href="#"><i class="fa fa-user"></i></a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<div class="main-content-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-2 left-menu">
+					<h3>Food Menu </h3>
+					<ul class="list-unstyled">
+						<li><a href="#MadMonsterPlatter">MadMonster Platter</a></li>
+						<li><a href="#RegularGourmetBurgers">Regular & Gourmet Burgers</a></li>
+							<li><a href="#sandwiches">Sandwiches</a></li>
+						<li><a href="#ofTheLand">Of the Land</a></li>
+						<li><a href="#ofTheSea">Of the Sea</a></li>
+						<li><a href="#sides">Sides</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-7 food-menu" id="food-menu">
+					<div id="MadMonsterPlatter"></div>
+					<div class="row" >
+						<div class="col-sm-12">
+							<h3>MadMonster Platter</h3>
+							<img src="media/imgAll/21465_menu.jpg" alt="image" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>MadMonster Platter <br>
+								<small>Regular burger, side of bolognese poutine, half a chicken steak with garlic mushroom garnish</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 499.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-12">
+							<h3 id="RegularGourmetBurgers">Regular & Gourmet Burgers</h3>
+							<img src="media/imgAll/18022_menu.jpg" alt="image" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Naga Blast Burger<br>
+								<small>
+									Beef Patty drenched our home made Naga sauce and a layer of chedder cheese.</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Tandoori Chicken Burger<br>
+								<small>Chicken breast fillet marinated in smokey tandoori sauce with a layer of cheddar cheese</small>
+							</h4>
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Smokewood BBQ Burger<br>
+								<small>Beef Patty drenched in a tangy BBQ sauce and a layer of chedder cheese.</small>
+							</h4>
+
+						</div>
+						<div class="	col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Hawaiian Chicken Burger<br>
+								<small>Chicken breast fillet topped with a layer of grilled pineapple and cheese</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Garlic Mayo Burger<br>
+								<small>Beef Patty drenched in our signature Garlic Mayo comes with a slice of cheddar cheese</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>The Cuban Burger<br>
+								<small>
+									This one consists of a smoked chicken base and our rendition of a mixed mustard sauce topped with pickles and a fat juicy beef patty on cheese.</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Madame Lucy Burger<br>
+								<small>The sausage gravy is garnished with strips of beef bacon and molten cheese gently resting on a beef patty.</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Nutty Elvis Burger<br>
+								<small>A cheesy beef patty topped with thick crunchy peanut butter.</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Cheezuz Burger<br>
+								<small>Contains two layers of beef patty, two layers of cheese, two layers of beef bacon and a combination of our signature sauces.</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Smokin' Chick<br>
+								<small>1 layer of BBQ chicken patty, 1 layer of beef bacon, 1 layer of smoked chicken, cheese</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-8">
+							  
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							  
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-12">
+							<h3 id="sandwiches">Sandwiches</h3>
+							<img src="media/imgAll/18024_menu.jpg" alt="Food" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-12">
+							<h3 id="ofTheLand">Of the land</h3>
+							<img src="media/imgAll/18024_menu.jpg" alt="Food" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="col-sm-12">
+							<h3 id="ofTheSea">Of the Sea</h3>
+							<img src="media/imgAll/18024_menu.jpg" alt="Food" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+
+
+					<div class="row">
+						<div class="col-sm-12">
+							<h3 id="sides">Sides</h3>
+							<img src="media/imgAll/18024_menu.jpg" alt="Food" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-8">
+							<h4>Mighty Spicy Chick<br>
+								<small>2 layers of BBQ chicken patty, 1 layer of beef bacon, 1 layer of cheese, drizzle of sriracha and nagablastic sauce</small>
+							</h4>
+
+						</div>
+						<div class="col-sm-4 text-right price">
+							<a href="#">
+								Tk. 220.00
+								<i class="fa fa-plus-circle"></i>
+							</a>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="col-sm-2 rightSide">
+					<div class="row">
+						<div class="col-sm-12">
+							<h4>Your order
+								<i class="fa fa-bitbucket-square pull-right"></i>
+							</h4>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<p>
+								<i class="fa fa-clock-o"></i>
+								Delivery time: 1h
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<p>
+								<i class="fa fa-car"></i>
+								Delivery fee: Tk. 50.00
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<p>
+								<i class="fa fa-dollar"></i>
+								Online payment: Available
+							</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<p>
+								<i class="fa fa-shopping-cart"></i>
+								Delivery min.: Tk. 300.00
+							</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer id="footer-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					All rights reserved by <a href="#">emythmakers.com</a>.2015
+				</div>
+			</div>
+		</div>
+	</footer>
+
+<script type="text/javascript" src="common/jQuery-2.1.4/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="common/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="common/js/emythmakers.js"></script>
+	<script>
+		$(document).ready(function(){
+			var headerHeight = $("#header").height();
+			$('a[href^="#"]').on('click',function (e) {
+				e.preventDefault();
+
+				var target = this.hash;
+				var $target = $(target);
+
+				$('html, body').stop().animate({
+					'scrollTop': $target.offset().top-(headerHeight+55)
+				}, 900, 'swing', function () {
+					window.location.hash = target;
+				});
+			});
+		});
+	</script>
+</body>
+</html>
